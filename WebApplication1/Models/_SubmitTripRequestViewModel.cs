937f92be1f63850a29002326129d2242317d6334
Yubanaswa Chakraborty <yubanaswa@hotmail.com>
2017-01-30 16:28:02 +0000

    UI fixes

---
+++ WebApplication1/Models/_SubmitTripRequestViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class _SubmitTripRequestViewModel
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string CircuitName { get; set; }
+    }
+}
