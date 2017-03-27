4a295c119a571f87c53111fe76177b4ecb310548
Yubanaswa Chakraborty <yubanaswa@hotmail.com>
2017-03-27 15:38:38 +0100

    India page changes

--- WebApplication1/Controllers/IndiaController.cs
+++ WebApplication1/Controllers/IndiaController.cs
@@ -14,15 +14,16 @@ namespace WebApplication1.Controllers
         // GET: India
         public ActionResult Index()
         {
-            List<Destination> _destinations = null;
-            var blError = DestinationManager.GetDestinationsForCountry("India", out _destinations);
-            if (blError.ErrorCode != 0 || _destinations == null || _destinations.Count == 0)
-            {
-                throw new ApplicationException(blError.ErrorMessage);
-            }
+            //List<Destination> _destinations = null;
+            //var blError = DestinationManager.GetDestinationsForCountry("India", out _destinations);
+            //if (blError.ErrorCode != 0 || _destinations == null || _destinations.Count == 0)
+            //{
+            //    throw new ApplicationException(blError.ErrorMessage);
+            //}
 
-            var _model = new CircuitModelBase() { AllDestinations = _destinations, CircuitName = "India" };
-            return View(_model);
+            //var _model = new CircuitModelBase() { AllDestinations = _destinations, CircuitName = "India" };
+            //return View(_model);
+            return View();
         }
     }
 }
