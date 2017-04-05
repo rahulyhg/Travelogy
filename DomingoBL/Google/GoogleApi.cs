8901e33a9134c2fa2a2265894145fea6fd791c0d
Yubanaswa Chakraborty <yubanaswa@hotmail.com>
2017-04-05 09:52:10 +0100

    India changes

---
+++ DomingoBL/Google/GoogleApi.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomingoBL.Google
+{
+    
+    public class GoogleApi
+    {
+        public static string GoogleKey = "AIzaSyCSltrNf2Z914TjYdW-Q5ZknGPjB-BYP30";
+
+    }
+}
