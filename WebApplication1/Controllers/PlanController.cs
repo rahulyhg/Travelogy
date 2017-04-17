f6a44449c297820bdb79b1fee090bb87130101ce
Yubanaswa Chakraborty <yubanaswa@hotmail.com>
2017-04-17 09:28:51 +0100

    Trip planner

---
+++ WebApplication1/Controllers/PlanController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class PlanController : DomingoControllerBase
+    {
+        // GET: Plan
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult India()
+        {
+            return View();
+        }
+
+        public ActionResult Tanzania()
+        {
+            return View();
+        }
+
+        public ActionResult Russia()
+        {
+            return View();
+        }
+
+        public ActionResult Mongolia()
+        {
+            return View();
+        }
+    }
+}
